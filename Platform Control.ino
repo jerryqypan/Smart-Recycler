ff9a212c01e18da7a5c39b6a99e7fb58fc6ecb62
zhangmason <masonzhangmn@gmail.com>
2016-11-20 12:03:43 -0500

    Add files via upload

---
+++ Platform Control.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>  
+                                 
+Servo TiltServo;                             // Declare left and right servos
+int input = 2;                              //0 = trash, 1 = recycling, 2 = stationary
+ 
+void setup()                                 // Built-in initialization block
+{
+  Serial.begin(9600);
+  
+  TiltServo.attach(13);                      // Attach left signal to pin 13
+}  
+
+void loop() { // run over and over
+  if (Serial.available()>0){                // If something is available in the serial port
+     input = Serial.read() - 48;            // Account for ascii
+     if (input == 0) {
+      //Serial.print("0");
+      TiltServo.writeMicroseconds(1300);    // Tilt left then return to normal
+      delay(1500);
+      TiltServo.writeMicroseconds(1500);
+     }
+     if (input == 1) {
+      //Serial.print("1");
+      TiltServo.writeMicroseconds(1700);    // Tilt right then return to normal
+      delay(1500);
+      TiltServo.writeMicroseconds(1500);
+     }
+     input = 2;
+  }
+  delay(1500);
+}
